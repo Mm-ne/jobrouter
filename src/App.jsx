167f662c7ebbd249c8cdba78ac27297c7589db7a
Mm-ne <abdelmoumeneboulahbal@gmail.com>
2024-03-02 17:35:14 +0100

    [ADD] : Career Routes + each Career details + curency formatter

--- src/App.jsx
+++ src/App.jsx
@@ -10,22 +10,48 @@ import {
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Faq from './Pages/help/Faq';
-import Contact from './Pages/help/Contact';
+import Contact from './Pages/help/Contact'; 
+import Careers, { careersLoader } from './Pages/careers/Careers';
 
 
 /* layouts */
 import RootLayout from './layouts/RootLayout';
 import HelpLayout from './layouts/HelpLayout';
+import NotFound from './Pages/NotFound';
+import CareersLayout from './layouts/CarrersLayout'
+import CareerDetail, { careerDetailLoader } from './Pages/careers/CareerDetail';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<RootLayout />}>
+
       <Route index element={<Home />} />
       <Route path='about' element={<About />} />
+
       <Route path='help' element={<HelpLayout />}>
         <Route path='faq' element={<Faq />} />
         <Route path='contact' element={<Contact />}/>
       </Route>
+      
+
+      <Route path="careers" element={<CareersLayout />}>
+        <Route 
+          index 
+          element={<Careers />} 
+          loader={careersLoader} 
+        />
+        <Route 
+          path=':id'
+          element={<CareerDetail />} 
+          loader={careerDetailLoader}
+          
+        />
+      </Route>
+      
+      
+      <Route path="*" element={<NotFound />} />
+
+
     </Route>
   )
 );
